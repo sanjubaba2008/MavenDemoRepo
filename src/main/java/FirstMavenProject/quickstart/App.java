c77259aa396d32e828ca6328e80f36695011d069
Sanjaya Ghimire <sanjaya428@gmail.com>
2021-01-12 10:17:43 -0600

    first commit

---
+++ src/main/java/FirstMavenProject/quickstart/App.java
@@ -0,0 +1,15 @@
+package FirstMavenProject.quickstart;
+
+//import com.google.gson.Gson;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/FirstMavenProject/quickstart/AppTest.java
@@ -0,0 +1,20 @@
+package FirstMavenProject.quickstart;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
